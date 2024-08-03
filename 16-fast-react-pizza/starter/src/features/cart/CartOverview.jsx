03614cfaa2b60e3198944ea48242e53d2aec6cb7
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2024-08-04 00:36:21 +0300

    using flexbox

--- 16-fast-react-pizza/starter/src/features/cart/CartOverview.jsx
+++ 16-fast-react-pizza/starter/src/features/cart/CartOverview.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function CartOverview() {
   return (
-    <div className="px-4 py-4 bg-stone-800 uppercase text-stone-200 sm:px-6">
+    <div className="flex items-center justify-between px-4 py-4 bg-stone-800 uppercase text-stone-200 sm:px-6">
       <p className="space-x-4 text-stone-300 text-sm font-semibold sm:space-x-6 md:text-base">
         <span>23 pizzas</span>
         <span>$23.45</span>
--- 16-fast-react-pizza/starter/src/features/user/Username.jsx
+++ 16-fast-react-pizza/starter/src/features/user/Username.jsx
@@ -1,4 +1,4 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Jonas</div>;
+  return <div className="hidden text-sm font-semibold md:block">Jonas</div>;
 }
 export default Username;
