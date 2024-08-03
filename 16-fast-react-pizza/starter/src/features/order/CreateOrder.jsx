1cf8041228b8e499a95de3bdad985f4de41043e5
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2024-08-04 02:46:58 +0300

    reusing styles with apply

--- 16-fast-react-pizza/starter/src/features/order/CreateOrder.jsx
+++ 16-fast-react-pizza/starter/src/features/order/CreateOrder.jsx
@@ -45,13 +45,13 @@ function CreateOrder() {
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input type="text" name="customer" required className="input" />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input type="tel" name="phone" required className="input" />
             {formErrors?.phone && <p>{formErrors.phone}</p>}
           </div>
         </div>
@@ -59,12 +59,7 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input
-              type="text"
-              name="address"
-              required
-              className="rounded-full border border-stone-200 text-sm w-full px-4 py-2 transition-all duration-300 md:px-6 md:py-3 focus:outline-none focus:ring focus:ring-yellow-400"
-            />
+            <input type="text" name="address" required className="input" />
           </div>
         </div>
 
