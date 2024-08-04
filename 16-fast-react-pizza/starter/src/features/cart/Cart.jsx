25d1c8600bf1298307a5eaed05c3409cdbd650e9
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2024-08-05 01:31:21 +0300

    styling the cart

--- 16-fast-react-pizza/starter/src/features/cart/Cart.jsx
+++ 16-fast-react-pizza/starter/src/features/cart/Cart.jsx
@@ -1,5 +1,6 @@
 import LinkButton from "../../ui/LinkButton";
 import Button from "../../ui/Button";
+import CartItem from "./CartItem";
 
 const fakeCart = [
   {
@@ -29,16 +30,22 @@ function Cart() {
   const cart = fakeCart;
 
   return (
-    <div>
+    <div className="py-4 px-3">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2>Your cart, %NAME%</h2>
-
-      <div>
+      <h2 className="mt-7 text-xl text-stone-800 font-bold">
+        Your cart, %NAME%
+      </h2>
+      <ul className="mt-3 divide-y divide-stone-200">
+        {cart.map((item) => (
+          <CartItem item={item} key={item.pizzaId} />
+        ))}
+      </ul>
+      <div className="mt-6 space-x-2">
         <Button to="/order/new" type="primary">
           Order pizzas
         </Button>
-        <button>Clear cart</button>
+        <Button type="secondary">Clear cart</Button>
       </div>
     </div>
   );
--- 16-fast-react-pizza/starter/src/ui/Button.jsx
+++ 16-fast-react-pizza/starter/src/ui/Button.jsx
@@ -5,9 +5,12 @@ function Button({ children, disabled, to, type }) {
     "inline-block bg-yellow-400 font-semibold rounded-full hover:bg-yellow-300 text-stone-800 uppercase tracking-wide transition-colors duration-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
 
   const styles = {
-    primary: base + "px-4 py-3 md:px-6 md:py-4",
-    small: base + "px-4 py-2 text-xs md:px-5 md:py-2.5",
+    primary: base + " px-4 py-3 md:px-6 md:py-4",
+    small: base + " px-4 py-2 text-xs md:px-5 md:py-2.5",
+    secondary:
+      "inline-block font-semibold rounded-full border-2 border-stone-300 text-stone-400 hover:bg-stone-300 hover:text-stone-800 uppercase tracking-wide transition-colors duration-300 focus:outline-none focus:bg-stone-300 focus:text-stone-800 focus:ring focus:ring-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5",
   };
+
   if (to)
     return (
       <Link to={to} className={styles[type]}>
