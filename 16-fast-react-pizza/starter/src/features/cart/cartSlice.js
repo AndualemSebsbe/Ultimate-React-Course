47568fb9b6de63b4715d3a59942ca02a5572a299
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2024-08-06 06:39:56 +0300

    adding menu items, building the cart overview and cart page

--- 16-fast-react-pizza/starter/src/features/cart/cartSlice.js
+++ 16-fast-react-pizza/starter/src/features/cart/cartSlice.js
@@ -1,17 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  // cart: []
+  cart: [],
 
-  cart: [
-    {
-      pizzaId: 12,
-      name: "Meditteranean",
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-  ],
+  //   cart: [
+  //     {
+  //       pizzaId: 12,
+  //       name: "Meditteranean",
+  //       quantity: 2,
+  //       unitPrice: 16,
+  //       totalPrice: 32,
+  //     },
+  //   ],
 };
 
 const cartSlice = createSlice({
@@ -44,4 +44,19 @@ const cartSlice = createSlice({
   },
 });
 
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  clearCart,
+} = cartSlice.actions;
+
 export default cartSlice.reducer;
+
+export const getCart = (state) => state.cart.cart;
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((acc, item) => acc + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((acc, item) => acc + item.totalPrice, 0);
--- 16-fast-react-pizza/starter/src/features/menu/MenuItem.jsx
+++ 16-fast-react-pizza/starter/src/features/menu/MenuItem.jsx
@@ -1,8 +1,25 @@
 import { formatCurrency } from "../../utils/helpers";
 import Button from "../../ui/Button";
+import { useDispatch } from "react-redux";
+import { addItem } from "../cart/cartSlice";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const dispatch = useDispatch();
+
+  const handleAddToCart = (e) => {
+    e.preventDefault();
+
+    const newPizza = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice,
+    };
+
+    dispatch(addItem(newPizza));
+  };
 
   return (
     <li className="flex py-2 gap-4">
@@ -24,7 +41,11 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-          <Button type="small">Add to cart</Button>
+          {!soldOut && (
+            <Button type="small" onClick={handleAddToCart}>
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
