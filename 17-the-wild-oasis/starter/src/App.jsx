f7e4a07b9db96813f2f142c648a1a747f0cfaf93
Andualem Sebsbe <andualemsebsbe1@gmail.com>
2024-08-17 23:06:28 +0300

    feat: implement global styles with styled components

--- 17-the-wild-oasis/starter/src/App.jsx
+++ 17-the-wild-oasis/starter/src/App.jsx
@@ -1,47 +1,33 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
   background-color: yellow;
-`
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  margin: 20px;
-  cursor: pointer;
-`
-
-const Input = styled.input`
-  display: block;
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
-  margin: 20px;
-`
+`;
 
 const StyledApp = styled.main`
   padding: 20px;
   background-color: orangered;
-`
+`;
 
 function App() {
   return (
-    <StyledApp>
-      <H1>The Wild Oasis</H1>
-      <Button onClick={() => alert("Check in")}>Check in</Button>
-      <Button onClick={() => alert("Check out")}>Check out</Button>
-      <div style={{display: 'flex'}}>
-        <Input type="number" placeholder="Number of guests" />
-        <Input type="number" placeholder="Number of guests" />
-      </div>
-    </StyledApp>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert("Check in")}>Check in</Button>
+        <Button onClick={() => alert("Check out")}>Check out</Button>
+        <div style={{ display: "flex" }}>
+          <Input type="number" placeholder="Number of guests" />
+          <Input type="number" placeholder="Number of guests" />
+        </div>
+      </StyledApp>
+    </>
   );
 }
-export default App
+export default App;
